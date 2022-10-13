196a129fe435909090c3888f41530f146c5a56db
AriusII <dragonmouff@gmail.com>
2022-10-13 20:03:29 +0200

    some updates

--- src/resources/routes/players/players.router.ts
+++ src/resources/routes/players/players.router.ts
@@ -5,4 +5,18 @@ rPlayers.get('/', (req: express.Request, res: express.Response) => {
     res.send("Index of Players")
 })
 
+rPlayers.get('/pinfo/:user', async (req: express.Request, res: express.Response) => {
+    const user = req.params.user
+    console.log(user)
+    const resp = await gClient.Players.playerLookup(user)
+    res.send(resp)
+})
+
+rPlayers.get('/listing', (req: express.Request, res: express.Response) => {
+    gClient.Players.playersList(0,10,10).
+    then((resp) => {
+        res.send(resp)
+    })
+})
+
 export default rPlayers
--- src/resources/routes/users/users.router.ts
+++ src/resources/routes/users/users.router.ts
@@ -5,4 +5,17 @@ rUsers.get('/', (req: express.Request, res: express.Response) => {
     res.send("Index of Users")
 })
 
+rUsers.get('/uinfo/:user', async (req: express.Request, res: express.Response) => {
+    const user = req.params.user
+    console.log(user)
+    gClient.Users.userLookup(user).then((resp) => {
+        res.send(resp)
+    })
+})
+
+rUsers.get('/listing', async (req: express.Request, res: express.Response) => {
+    const anw = await gClient.Users.usersList(1,10,10)
+    res.send(anw)
+})
+
 export default rUsers
