ae98b58d9795f6ade2e258e2eaab8b780f8fa4ae
WeylonSantana <weylonsantanaoff@gmail.com>
2022-10-14 10:58:28 -0300

    Admin and Chat Router

--- src/resources/routes/admin/admin.router.ts
+++ src/resources/routes/admin/admin.router.ts
@@ -1,8 +1,250 @@
 import express from 'express'
 const rAdmin = express.Router()
 
-rAdmin.get('/', (req: express.Request, res: express.Response) => {
-    res.send("Index of Admin")
-})
+//#region Players Actions
+
+rAdmin.post(
+  '/ban-player',
+  async (req: express.Request, res: express.Response) => {
+    const { player, duration, reason, moderator, ip } = req.body
+
+    if (!player || !duration || !reason || !moderator || ip === undefined) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.banPlayer(
+      player,
+      duration,
+      reason,
+      moderator,
+      ip
+    )
+
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/mute-player',
+  async (req: express.Request, res: express.Response) => {
+    const { player, duration, reason, moderator, ip } = req.body
+
+    if (!player || !duration || !reason || !moderator || ip === undefined) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.mutePlayer(
+      player,
+      duration,
+      reason,
+      moderator,
+      ip
+    )
+
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/kick-player',
+  async (req: express.Request, res: express.Response) => {
+    const { player } = req.body
+
+    if (!player) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.kickPlayer(player)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/kill-player',
+  async (req: express.Request, res: express.Response) => {
+    const { player } = req.body
+
+    if (!player) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.killPlayer(player)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/warp-player',
+  async (req: express.Request, res: express.Response) => {
+    const { player, mapId } = req.body
+
+    if (!player || !mapId) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.playerWarpTo(player, mapId)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/warp-player-location',
+  async (req: express.Request, res: express.Response) => {
+    const { player, mapId, x, y } = req.body
+
+    if (!player || !mapId || !x || !y) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.playerWarpToLoc(player, mapId, x, y)
+    res.send(resp)
+  }
+)
+
+//#endregion
+
+//#region Users Actions
+
+rAdmin.post(
+  '/ban-user',
+  async (req: express.Request, res: express.Response) => {
+    const { user, duration, reason, moderator, ip } = req.body
+
+    if (!user || !duration || !reason || !moderator || ip === undefined) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.banUser(
+      user,
+      duration,
+      reason,
+      moderator,
+      ip
+    )
+
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/mute-user',
+  async (req: express.Request, res: express.Response) => {
+    const { user, duration, reason, moderator, ip } = req.body
+
+    if (!user || !duration || !reason || !moderator || ip === undefined) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.muteUser(
+      user,
+      duration,
+      reason,
+      moderator,
+      ip
+    )
+
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/unban-user',
+  async (req: express.Request, res: express.Response) => {
+    const { user } = req.body
+
+    if (!user) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.unbanUser(user)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/unmute-user',
+  async (req: express.Request, res: express.Response) => {
+    const { user } = req.body
+
+    if (!user) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.unmuteUser(user)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/kick-user',
+  async (req: express.Request, res: express.Response) => {
+    const { user } = req.body
+
+    if (!user) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.kickUser(user)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/kill-user',
+  async (req: express.Request, res: express.Response) => {
+    const { user } = req.body
+
+    if (!user) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.killUser(user)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/warp-user',
+  async (req: express.Request, res: express.Response) => {
+    const { user, mapId } = req.body
+
+    if (!user || !mapId) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.userWarpTo(user, mapId)
+    res.send(resp)
+  }
+)
+
+rAdmin.post(
+  '/warp-user-location',
+  async (req: express.Request, res: express.Response) => {
+    const { user, mapId, x, y } = req.body
+
+    if (!user || !mapId || !x || !y) {
+      res.sendStatus(400)
+      return
+    }
+
+    const resp = await gClient.Admin.userWarpToLoc(user, mapId, x, y)
+    res.send(resp)
+  }
+)
+
+//#endregion
 
 export default rAdmin
--- src/resources/routes/chat/chat.router.ts
+++ src/resources/routes/chat/chat.router.ts
@@ -1,8 +1,40 @@
 import express from 'express'
 const rChat = express.Router()
 
-rChat.get('/', (req: express.Request, res: express.Response) => {
-    res.send("Index of Chat")
+rChat.post('/direct', async (req: express.Request, res: express.Response) => {
+  const { user, message, color, target } = req.body
+
+  if (!user || !message) {
+    res.sendStatus(400)
+    return
+  }
+
+  const resp = await gClient.Chat.directMessage(user, message, color, target)
+  res.send(resp)
+})
+
+rChat.post('/map', async (req: express.Request, res: express.Response) => {
+  const { mapId, message, color, target } = req.body
+
+  if (!mapId || !message) {
+    res.sendStatus(400)
+    return
+  }
+
+  const resp = await gClient.Chat.proximityMessage(mapId, message, color, target)
+  res.send(resp)
+})
+
+rChat.post('/global', async (req: express.Request, res: express.Response) => {
+  const { message, color, target } = req.body
+
+  if (!message) {
+    res.sendStatus(400)
+    return
+  }
+
+  const resp = await gClient.Chat.globalMessage(message, color, target)
+  res.send(resp)
 })
 
 export default rChat
